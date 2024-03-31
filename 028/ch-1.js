4dc1f990ceecacbd968f2d6bae34f06022f1818d
Andrew Shitov <mail@andreyshitov.com>
2024-03-31 19:12:45 +0200

    Week 28 Task 1

---
+++ 028/ch-1.js
@@ -0,0 +1,8 @@
+// Solution of Task 1 of The Weekly Challenge 28
+// https://theweeklychallenge.org/blog/perl-weekly-challenge-028/
+
+// $ node ch-1.js
+// 19:12:26
+
+let now = new Date();
+console.log(now.toLocaleTimeString('en-US', {hour12: false}));
